b9e89732f5900a15f39a045c3addb8fb867a6a7e
Noel <nk.agile@gmail.com>
2012-05-02 16:34:09 +0100

    Added support for partial function application

---
+++ Scalesque.Tests/FunctionApplication/When_creating_partially_applied_functions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Scalesque.FunctionApplication {
+    
+    public class When_creating_partially_applied_functions : UnitTestBase {
+        private Func<int, int, int, int, int, int, int, int> func;
+        private Func<int, int> apply6Params;
+
+        public override void Given() {
+            func = (v1, v2, v3, v4, v5, v6, v7) => v1 + v2 + v3 + v4 + v5 + v6 + v7;
+        }
+
+        public override void Because() {
+            apply6Params = func.Partial(1, 1, 1, 1, 1, 1);
+        }
+
+        [Test]
+        public void It_should_have_applied_the_first_six_parameters_creating_a_function_of_arity_1() {
+            apply6Params(1).Should().Be(7); 
+        }
+    }
+}
--- Scalesque.net40/Properties/AssemblyInfo.cs
+++ Scalesque.net40/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scalaesque")]
+[assembly: AssemblyTitle("Scalesque")]
 [assembly: AssemblyDescription("Functional programming library for c#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
